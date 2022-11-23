cc8ed8c3f3efe24dbbb6de3ba6f9bf955510df77
Vamsi Chunduru <cvamsikrishna11@gmail.com>
2022-11-23 18:11:09 -0500

    setting up the project codebase

--- jenkins-maven-ansible-setup.sh
+++ jenkins-maven-ansible-setup.sh
@@ -13,10 +13,13 @@ sudo systemctl start jenkins
 # Installing Git
 sudo yum install git -y
 
-# Installing maven
-sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-sudo yum install -y apache-maven
+# Installing maven - commented out as usage of tools explanation is required.
+# sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+# sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+# sudo yum install -y apache-maven
+
+# Java installation
+# sudo yum install java-1.8.0-openjdk -y
 
 # Installing Ansible
 sudo amazon-linux-extras install ansible2 -y
