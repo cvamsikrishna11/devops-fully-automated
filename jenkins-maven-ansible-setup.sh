be34e0de9a63f6944e935b0fd61356003ac43bf1
Vamsi Chunduru <cvamsikrishna11@gmail.com>
2022-11-24 20:41:29 -0500

    installations

--- jenkins-maven-ansible-setup.sh
+++ jenkins-maven-ansible-setup.sh
@@ -31,4 +31,3 @@ sudo echo ansadmin:ansadmin | chpasswd
 sudo sed -i "s/.*#host_key_checking = False/host_key_checking = False/g" /etc/ansible/ansible.cfg
 sudo sed -i "s/.*#enable_plugins = host_list, virtualbox, yaml, constructed/enable_plugins = aws_ec2/g" /etc/ansible/ansible.cfg
 sudo ansible-galaxy collection install amazon.aws
-sudo mkdir -p /opt/ansible/inventory
