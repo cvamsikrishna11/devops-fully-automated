460381bb0336656d0e30b2a5df83937e904571b4
Vamsi Chunduru <cvamsikrishna11@gmail.com>
2023-04-11 14:17:54 -0400

    modifying the installation document slightly as the new version of ec2 ami 2023 dont support linux-extras suggested to use the old one

--- jenkins-maven-ansible-setup.sh
+++ jenkins-maven-ansible-setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Hardware requirements: AWS Linux 2 with mimum t2.medium type instance & port 8080(jenkins), 9100 (node-exporter) should be allowed on the security groups
+# Hardware requirements: Amazon Linux 2 Kernel 5.10 AMI with mimum t2.medium type instance & port 8080(jenkins), 9100 (node-exporter) should be allowed on the security groups
+#Note: Dont use the latest EC2 Linux of 2023 as it doesnt have amazon-linux-extras which was used in the Jenkins installation as per the old AMI.
 # Installing Jenkins
 sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key # Note: Refer this link to change this key line frequently https://pkg.jenkins.io/redhat-stable/
 sudo yum upgrade
 sudo amazon-linux-extras install java-openjdk11 -y
 sudo yum install jenkins -y
